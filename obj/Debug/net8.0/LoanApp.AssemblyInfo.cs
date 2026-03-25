b563c1d03d3fc3af6be7dbba5a46c4e5ffe65aa8
itsEphraim <okonofuahephraim@gmail.com>
2026-03-25 18:28:45 +0000

    Text colour change on loan Amount on Admin Application Details

--- obj/Debug/net8.0/LoanApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LoanApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7491d2e578d6c56bd3f04ad32832b0ffc58c86ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dc0e40ccfb7b22f3a478728cfa1d188cd538e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
