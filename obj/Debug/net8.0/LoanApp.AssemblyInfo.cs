34e070209b339fbf58b772df2aa14c71fe806da0
itsEphraim <okonofuahephraim@gmail.com>
2026-03-28 16:07:51 +0000

    gitignore added

--- obj/Debug/net8.0/LoanApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/LoanApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoanApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dc0e40ccfb7b22f3a478728cfa1d188cd538e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77786c3430b2a9a98f58eb7956eaf5a1b6da8f01")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoanApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoanApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
